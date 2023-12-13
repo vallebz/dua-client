e559fe12be9ee5c78b03d97f7901c87fe690a3a9
vallebz <80699091+vallebz@users.noreply.github.com>
2023-12-13 13:24:23 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Demo Button :)</h1>
+    <p>{{ displayText }}</p>
+    <button @click="addText">Nächster Request</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      displayText: "",
+    };
+  },
+  methods: {
+    async addText() {
+      console.log("addText");
+      try {
+        const response = await fetch('http://localhost:8080/jakarta-example/resource', {
+          method: "GET",
+          // headers: {
+          //   "RDF-Data": "@prefix ex: <http://example.org> . @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> . ex:Valentin a ex:Person ; a ex:Human .",
+          // },
+        });
+        console.log("try response");
+
+        const data = await response.text();
+
+        this.displayText += ` ${data} `;
+      } catch (error) {
+        console.error += ('Error during fetch:', error);
+        this.displayText += ` Fetch-Fehler: ${error.message} `;
+      }
+    },
+  },
+};
+</script>
