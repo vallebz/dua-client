c01e6c10a8cc3e8dc9f6838e0ec7081da33a0bc5
vallebz <80699091+vallebz@users.noreply.github.com>
2023-12-15 16:42:41 +0100

    added display of response, response headers

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,87 @@
 <template>
   <div>
-    <h1>Demo Button :)</h1>
-    <p>{{ displayText }}</p>
-    <button @click="addText">Nächster Request</button>
+    <h1>DUA Demo</h1>
+    <h2>Example of the protocol sequence</h2>
+    <div id="firstRequest"></div>
+    <br>
+    <div id="secondRequest"></div>
+    <br>
+    <button id="addText" @click="addText">{{ buttonText }}</button>
+    <button @click="doReset">Reset</button>
   </div>
 </template>
 
 <script>
 export default {
+
   data() {
     return {
-      displayText: "",
+      firstRequest: "",
+      secondRequest: "",
+      buttonText: "Send HTTP request",
+      counter: 0,
+      url: "http://localhost:8080/jakarta-example/resource",
     };
   },
+
   methods: {
+
     async addText() {
-      console.log("addText");
-      try {
-        const response = await fetch('http://localhost:8080/jakarta-example/resource', {
+
+      if (this.counter == 0) {
+
+        const { bodyData, headerData } = await this.fetchData(this.url);
+        document.getElementById('firstRequest').innerHTML += bodyData + "</br>";
+        document.getElementById('firstRequest').innerHTML += headerData;
+        this.buttonText = "Send HTTP request with credential attached";
+        this.counter += 1;
+        return;
+
+      } else if (this.counter == 1) {
+        const options = {
           method: "GET",
           headers: {
             "authn-data": "@prefix ex: <http://example.org> . @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> . @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> . ex:Valentin a ex:Person ; a ex:Human .",
           },
+        };
+        const { bodyData, headerData } = await this.fetchData(this.url, options);
+        document.getElementById('secondRequest').innerHTML += bodyData + "</br>";
+        document.getElementById('secondRequest').innerHTML += headerData;
+        document.getElementById('addText').style.display = "none";
+        this.counter += 1;
+        return;
+      }
+    },
+
+    doReset() {
+      document.getElementById('firstRequest').innerHTML = "";
+      document.getElementById('secondRequest').innerHTML = "";
+      document.getElementById('addText').style.display = "inline";
+      this.buttonText = "Send HTTP request";
+      this.counter = 0;
+    },
+
+    async fetchData(url, options = {}) {
+      try {
+        const response = await fetch(url, options);
+        const bodyData = await response.text();
+        let headerData = '<b>Headers:</b></br>';
+        response.headers.forEach((value, name) => {
+          headerData += `${name}: ${value}</br>`;
         });
-        console.log("try response");
-
-        const data = await response.text();
-
-        this.displayText += ` ${data} `;
+        return { bodyData, headerData };
       } catch (error) {
-        console.error += ('Error during fetch:', error);
+        console.error('Error during fetch:', error);
         this.displayText += ` Fetch-Fehler: ${error.message} `;
+        return null;
       }
     },
   },
 };
 </script>
+
+<style>
+button {
+      margin-right: 10px;
+    }
+</style>
